3f3384064c31cdf5c09ceeee45262d391917ddd2
hemisemidemipresent <hnngggrrrr@gmail.com>
2024-05-29 18:04:25 +0800

    Create infiniteloop2.py

---
+++ infiniteloop2.py
@@ -0,0 +1,310 @@
+from manim import *
+import math
+import numpy as np
+
+# set frame rate
+config.frame_rate = 60.0
+
+# manim converted units (x btd6 units = 1 manim unit)
+x = 30
+R = 80/x
+r = 40/x
+v_p = 1.25/x # shredder
+v_c = 1.2 * v_p 
+threshold_distance_squared = 0.1/(x**2)
+reverse_phase_change = PI/5  # unchanged
+max_delta_angle = 0.105  # unchanged
+P = TAU*R/(v_p*60)  # period
+
+# util functions
+def constrain_to_pi(angle):
+    while angle > PI:
+        angle -= TAU
+    while angle < -PI:
+        angle += TAU
+    return angle
+
+
+def constrain_to_tau(angle):
+    return angle % TAU
+
+
+fig8_1 = Circle(radius=r, color=GREY)
+fig8_2 = Circle(radius=r, color=GREY)
+fig8_1.rotate(PI*2)
+fig8_1.move_to(r*UP)
+fig8_2.move_to(r*DOWN)
+fig8 = Group(fig8_1, fig8_2)
+
+figinf_1 = Circle(radius=r, color=GREY)
+figinf_2 = Circle(radius=r, color=GREY)
+figinf_1.move_to(r*RIGHT)
+figinf_2.move_to(r*LEFT)
+figinf = Group(figinf_1, figinf_2)
+
+circle = Circle(radius=R, color=GREY)
+
+
+def circlephase(phase):
+    return circle.point_at_angle(-phase)
+
+
+def fig8phase(phase):
+    phase = constrain_to_tau(phase)
+    if phase <= PI:
+        return fig8_1.point_at_angle(3/2*PI - 2*phase)
+    else:
+        return fig8_2.point_at_angle(-3/2*PI + 2*phase)
+
+
+def figinfphase(phase):
+    phase = constrain_to_tau(phase)
+    if phase <= PI:
+        return figinf_1.point_at_angle(PI - 2*phase)
+    else:
+        return figinf_2.point_at_angle(2*phase)
+
+
+def target(targeting, phase):
+    if targeting == 'Circle':
+        target_point = circlephase(phase)
+    elif targeting == '8':
+        target_point = fig8phase(phase)
+    elif targeting == 'Infinity':
+        target_point = figinfphase(phase)
+    return target_point
+
+# ------------------------------------------------------------ #
+# ace configuration (initial)                                  #
+# ------------------------------------------------------------ #
+
+targeting = "Circle"
+
+# done in degrees lol
+events = {
+ 229: 'Infinity',
+ 273: '8',
+ 295: 'Circle',
+ 308: 'Infinity',
+ 333: '8',
+ 361: 'Circle',
+ 375: 'Infinity',
+ 393: '8',
+ 465: 'Circle',
+ 621: 'Infinity',
+ 661: '8',
+ 680: 'Circle',
+ 692: 'Infinity',
+ 713: '8',
+ 738: 'Circle',
+ 752: 'Infinity',
+ 773: '8',
+ 799: 'Circle',
+ 812: 'Infinity',
+ # reverse
+ 837: '8',
+ 859: 'Infinity',
+ 866: '8',
+ 883: 'Circle',
+ 898: 'Infinity',
+ 925: '8',
+ 944: 'Circle',
+ 950: 'Infinity',
+ 960: '8',
+ 968: 'Circle',
+}
+onPath = True
+reverse = 1  # 1 = Normal, -1 = Reverse
+v_angle = 0  # if onPath initially true no need to set this except the first frame will be fucked
+initial_phase = 0.515303 * TAU
+final_phase = initial_phase + 816 * v_p/R
+
+i = -1
+
+# almost always False
+justOnPath = False
+
+initial_location = target(targeting, initial_phase)
+# initial_location = [30/80*R, 0, 0]
+
+
+# more code
+phase = ValueTracker(initial_phase)
+
+circledot = Dot(color=RED)
+circledot.add_updater(lambda x: x.move_to(circlephase(phase.get_value())))
+fig8dot = Dot(color=GREEN)
+fig8dot.add_updater(lambda x: x.move_to(fig8phase(phase.get_value())))
+figinfdot = Dot(color=BLUE)
+figinfdot.add_updater(lambda x: x.move_to(figinfphase(phase.get_value())))
+if onPath:
+    color = YELLOW
+else:
+    color = WHITE
+ace = Dot(point=initial_location, color=color)
+
+def acemove(ace, phase, line):
+    global targeting
+    global onPath
+    global v_angle
+    global justOnPath
+    global i
+    
+    i += 1
+    
+    target_point = target(targeting, phase)
+
+    def applyEvent(phase):
+        global targeting
+        global onPath
+        global events
+        global i
+        
+        if i in events:
+            event = events[i]
+            onPath = False
+            ace.set_fill(WHITE)
+            targeting = event
+            # del events[phase_deg]
+            
+    def updatePath(new_pos):
+        global path
+        global justOnPath
+        if i == 600:
+            new_path = VMobject(stroke_color=YELLOW)
+            new_path.set_points_as_corners([ace.get_center(), ace.get_center()])
+            path.become(new_path)
+            return
+        previous_path = path.copy()
+        previous_path.add_points_as_corners([new_pos])
+        path.become(previous_path)
+        
+    if onPath:
+        if justOnPath:
+            ace.set_fill(YELLOW)  # visual indicator
+            justOnPath = False
+        phi = constrain_to_tau(phase)
+        if targeting == 'Circle':
+            v_angle = -PI/2 - phi
+        elif targeting == '8':
+            if phi < PI:
+                v_angle = PI - 2*phi
+            else:
+                v_angle = 2*(phi-PI) - PI
+        elif targeting == 'Infinity':
+            if phi < PI:
+                v_angle = PI/2 - 2*phi
+            else:
+                v_angle = 2*phi + PI/2
+        if reverse == -1:
+            v_angle -= PI
+
+        applyEvent(phase)
+        updatePath(target_point)
+        return target_point
+
+    ace_position = ace.get_center()
+
+    # onPath check
+    
+
+    # move the ace
+    delta_x = target_point[0] - ace_position[0]
+    delta_y = target_point[1] - ace_position[1]
+    angle = math.atan2(delta_y, delta_x)
+    angle_diff = constrain_to_pi(angle - v_angle)
+
+    if abs(angle_diff) < max_delta_angle:
+        v_angle += angle_diff
+    else:
+        v_angle += np.sign(angle_diff) * max_delta_angle
+    # v_angle = constrain_to_pi(v_angle)
+
+    line.put_start_and_end_on(ace_position, target_point)
+
+    applyEvent(phase)
+    new_pos = ace_position + [v_c*math.cos(v_angle), v_c*math.sin(v_angle), 0]
+    updatePath(new_pos)
+    
+    d_squared = (target_point[0] - new_pos[0]
+                 )**2 + (target_point[1] - new_pos[1])**2
+    if d_squared < threshold_distance_squared:
+        onPath = True
+        justOnPath = True
+        print(phase)
+        # extremely scuffed method to hide the line
+        line.put_start_and_end_on([999, 0, 0], [999, 1, 0])
+    
+    return new_pos
+ace.add_updater(lambda x: x.move_to(acemove(x, phase.get_value(), line)))
+
+
+# for the ace path
+path = VMobject(stroke_color=YELLOW)
+path.set_points_as_corners([ace.get_center(), ace.get_center()])
+def update_path(path):
+    previous_path = path.copy()
+    previous_path.add_points_as_corners([ace.get_center()])
+    path.become(previous_path)
+# path.add_updater(update_path)
+
+# velocity arrow
+varrow = Arrow(start=ace.get_center(), end=ace.get_center() + [math.cos(v_angle), math.sin(v_angle), 0])
+def update_varrow(varrow):
+    arrow_length = 0.5  # arrow length of 0.5
+    varrow.put_start_and_end_on(ace.get_center(), ace.get_center() + [math.cos(v_angle) * arrow_length, math.sin(v_angle) * arrow_length, 0])
+varrow.add_updater(update_varrow)
+
+line = Line(start=[0, 0, 0], end=[1e-9, 0, 0])
+
+
+text = Text(str(int(phase.get_value()/DEGREES))).shift(3*RIGHT)
+text.add_updater(lambda t: t.become(Text(str(int(phase.get_value()/DEGREES)))))
+
+class CreateCircle(Scene):
+    def construct(self):
+        global initial_phase
+        global final_phase
+        global reverse
+        global onPath
+        global events
+        
+        
+        self.add(fig8)
+        self.add(figinf)
+        self.add(circle)
+        self.add(fig8dot)
+        self.add(figinfdot)
+        self.add(circledot)
+        
+        self.add(line)
+        self.add(path)
+        self.add(varrow)
+        self.add(ace)
+        
+        run_time = abs(final_phase-initial_phase)/TAU * P
+        
+        
+        phase_change = phase.animate(rate_func=linear, run_time=run_time).set_value(final_phase)        
+        self.play(phase_change)
+
+
+        # reverse animation
+        ace.clear_updaters() # freeze ace position
+        varrow.clear_updaters()
+        initial_phase = phase.get_value()-reverse_phase_change
+        phase.set_value(initial_phase) # move the circledot back
+        
+        # final_phase = initial_phase - (1116- 816) * v_p/R
+        final_phase = initial_phase - 0.9
+        
+        # manipulate state
+        reverse = -reverse
+        onPath = False
+        
+        # return
+        ace.add_updater(lambda x: x.move_to(
+            acemove(x, phase.get_value(), line)))
+        varrow.add_updater(update_varrow)
+
+        self.play(phase.animate.set_value(final_phase), rate_func=linear, run_time=abs(final_phase-initial_phase)/TAU * P)
